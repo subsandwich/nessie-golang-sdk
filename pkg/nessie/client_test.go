dd90dab8c3369432633c5f17055c5cc4889ffe76
jake612 <jake612@live.unc.edu>
2023-09-14 20:57:19 -0400

    Refactor client_test

--- pkg/nessie/client_test.go
+++ pkg/nessie/client_test.go
@@ -7,21 +7,39 @@ import (
 )
 
 func TestClient(t *testing.T) {
-	testClient := Client{
-		baseURL: "http://abc.com",
-		apiKey:  "123",
+	testCases := []struct {
+		Name      string
+		BaseUrl   string
+		Params    *map[string]string
+		TargetUrl string
+	}{
+		{
+			Name:      "No params",
+			BaseUrl:   "http://abc.com",
+			TargetUrl: "http://abc.com/path?key=123",
+		},
+		{
+			Name:    "With Params",
+			BaseUrl: "http://abc.com",
+			Params: &map[string]string{
+				"arg":  "1",
+				"arg2": "2",
+			},
+			TargetUrl: "http://abc.com/path?arg=1&arg2=2&key=123",
+		},
 	}
 
-	url, err := testClient.createURL("path", nil)
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			testClient := Client{
+				baseURL: tc.BaseUrl,
+				apiKey:  "123",
+			}
 
-	assert.NoError(t, err)
-	assert.Equal(t, "http://abc.com/path?key=123", url)
+			url, err := testClient.createURL("path", tc.Params)
 
-	url, err = testClient.createURL("path", &map[string]string{
-		"arg":  "1",
-		"arg2": "2",
-	})
-
-	assert.NoError(t, err)
-	assert.Equal(t, "http://abc.com/path?arg=1&arg2=2&key=123", url)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.TargetUrl, url)
+		})
+	}
 }
